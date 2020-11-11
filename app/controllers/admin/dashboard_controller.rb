0dc3fe2cedcd1c018cd99bc72fe8aa4d96b3ae8b
vagrant <shadmerhi@gmail.com>
2020-11-11 06:33:18 +0000

    Added http basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: Rails.configuration.admin[:name], password: Rails.configuration.admin[:password]
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: Rails.configuration.admin[:name], password: Rails.configuration.admin[:password]
+
   def index
     @products = Product.order(id: :desc).all
   end
