7545c3644c84bf0db01f1538b448399c20be8a0d
vagrant <shadmerhi@gmail.com>
2020-11-11 08:14:25 +0000

    created instance variables for products and categories for show view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin[:name], password: Rails.configuration.admin[:password]
 
   def show
+    @product_count = Product.count;
+    @categories = Category.all;
   end
 end
