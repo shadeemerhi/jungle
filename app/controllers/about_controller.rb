f232aaf2a0a469bbd70bf953b871464b09ffe2ba
vagrant <shadmerhi@gmail.com>
2020-11-11 18:59:10 +0000

    changed query from 'where' to 'find_by' to access single element instead of array

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,8 +1,7 @@
 class AboutController < ApplicationController
 
   def index
-    @product = Product.where(name: 'Optimal Sleeping Bed')
-    puts @product.inspect
+    @product = Product.find_by(name: 'Optimal Sleeping Bed')
   end
   
 end
