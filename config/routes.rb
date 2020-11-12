849c45b3bcad6ad45f03adbfd4c5109a8c0265b5
vagrant <shadmerhi@gmail.com>
2020-11-12 18:30:23 +0000

    session routes added for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
