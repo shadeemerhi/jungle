70382d88624d534db59eeb6719a47d9fd8c38f7f
vagrant <shadmerhi@gmail.com>
2020-11-12 19:11:00 +0000

    login/signup functioning

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+
+  has_secure_password
+
 end
