832ccda6b262094592b240777d07f3ff69490af5
vagrant <shadmerhi@gmail.com>
2020-11-17 02:46:33 +0000

    user email downcase prior to saving to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,21 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, confirmation: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  before_save :downcase_email
 
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email((email.strip).downcase)
+    if user && user.authenticate(password)
+      user
+    end
+  end
+
+  def downcase_email
+    self.email.downcase!
+  end
 
 end
